0d4c9da21891f4d3acc211f2ab97ad310b010b55
Drew Wilken <wilk0001@stthomas.edu>
2018-04-04 09:23:21 -0500

    Pipeline stage functions and indentation fixes

--- pipeline.c
+++ pipeline.c
@@ -23,51 +23,51 @@
 #define NOOPINSTRUCTION 0x1c00000
 
 typedef struct IFIDstruct{
-  int instr;
-  int pcplus1;
+    int instr;
+    int pcplus1;
 } IFIDType;
 
 typedef struct IDEXstruct{
-  int instr;
-  int pcplus1;
-  int readregA;
-  int readregB;
-  int offset;
+    int instr;
+    int pcplus1;
+    int readregA;
+    int readregB;
+    int offset;
 } IDEXType;
 
 typedef struct EXMEMstruct{
-  int instr;
-  int branchtarget;
-  int aluresult;
-  int readreg;
+    int instr;
+    int branchtarget;
+    int aluresult;
+    int readreg;
 } EXMEMType;
 
 typedef struct MEMWBstruct{
-  int instr;
-  int writedata;
+    int instr;
+    int writedata;
 } MEMWBType;
 
 typedef struct WBENDstruct{
-  int instr;
-  int writedata;
+    int instr;
+    int writedata;
 } WBENDType;
 
 typedef struct statestruct{
-  int pc;
-  int instrmem[NUMMEMORY];
-  int datamem[NUMMEMORY];
-  int reg[NUMREGS];
-  int numMemory;
-  IFIDType IFID;
-  IDEXType IDEX;
-  EXMEMType EXMEM;
-  MEMWBType MEMWB;
-  WBENDType WBEND;
-  int cycles; /* Number of cycles run so far */
-  int fetched; /* Total number of instructions fetched */
-  int retired; /* Total number of completed instructions */
-  int branches; /* Total number of branches executed */
-  int mispreds; /* Number of branch mispredictions*/
+    int pc;
+    int instrmem[NUMMEMORY];
+    int datamem[NUMMEMORY];
+    int reg[NUMREGS];
+    int numMemory;
+    IFIDType IFID;
+    IDEXType IDEX;
+    EXMEMType EXMEM;
+    MEMWBType MEMWB;
+    WBENDType WBEND;
+    int cycles; /* Number of cycles run so far */
+    int fetched; /* Total number of instructions fetched */
+    int retired; /* Total number of completed instructions */
+    int branches; /* Total number of branches executed */
+    int mispreds; /* Number of branch mispredictions*/
 } statetype;
 
 int field0(int instruction);
@@ -77,6 +77,15 @@ int opcode(int instruction);
 void printinstruction(int instr);
 void printstate(statetype *stateptr);
 int signextend(int num);
+
+/*
+ * Pipeline stage functions
+ */
+void IFStage(state* state, state* newstate);
+void IDStage(state* state, state* newstate);
+void EXStage(state* state, state* newstate);
+void MEMStage(state* state, state* newstate);
+void WBStage(state* state, state* newstate);
 
 statetype state;
 statetype newstate;
@@ -90,136 +99,134 @@ int main(int argc, char *argv[]){
  
  //stateType state;
 
- while(1){
-  printstate(&state);
-  /* check for halt */
-  if(HALT == opcode(state.MEMWB.instr)) {
-      printf("machine halted\n");
-      printf("total of %d cycles executed\n", state.cycles);
-      printf("total of %d instructions fetched\n", state.fetched);
-      printf("total of %d instructions retired\n", state.retired);
-      printf("total of %d branches executed\n", state.branches);
-      printf("total of %d branch mispredictions\n", state.mispreds);
-      exit(0);
-    }//if halt
-  
-  newstate = state;
-  newstate.cycles++;
+    while(1){
+        printstate(&state);
+        /* check for halt */
+        if(HALT == opcode(state.MEMWB.instr)) {
+            printf("machine halted\n");
+            printf("total of %d cycles executed\n", state.cycles);
+            printf("total of %d instructions fetched\n", state.fetched);
+            printf("total of %d instructions retired\n", state.retired);
+            printf("total of %d branches executed\n", state.branches);
+            printf("total of %d branch mispredictions\n", state.mispreds);
+            exit(0);
+        }//if halt
 
-  /*------------------ IF stage ----------------- */
+        newstate = state;
+        newstate.cycles++;
 
-  //newstate ifstage(state state)
+        /*------------------ IF stage ----------------- */
+        int IFStage(state* state, state* newstate);
 
-  /*------------------ ID stage ----------------- */
 
-  //newstate idstage(state state)
+        /*------------------ ID stage ----------------- */
+        
 
-  /*------------------ EX stage ----------------- */
+        //newstate idstage(state state)
 
-  //newstate exstage(state state)
+        /*------------------ EX stage ----------------- */
 
-  /*------------------ MEM stage ----------------- */
 
-  //newstate memstage(state state)
+        /*------------------ MEM stage ----------------- */
 
-  /*------------------ WB stage ----------------- */
+
+        /*------------------ WB stage ----------------- */
 
   //newstate wbstage(state state)
 
-  state = newstate; /* this is the last statement before the end of the loop.
+        state = newstate; /* this is the last statement before the end of the loop.
                     It marks the end of the cycle and updates the current
                     state with the values calculated in this cycle
                     – AKA “Clock Tick”. */
-
-  }//while
+    }//while
 }//main
 
 
 int field0(int instruction){
- return( (instruction>>19) & 0x7);
+    return( (instruction>>19) & 0x7);
 }
 
 int field1(int instruction){
- return( (instruction>>16) & 0x7);
+    return( (instruction>>16) & 0x7);
 }
 
 int field2(int instruction){
- return(instruction & 0xFFFF);
+    return(instruction & 0xFFFF);
 }
 
 int opcode(int instruction){
- return(instruction>>22);
+    return(instruction>>22);
 }
 
 void printinstruction(int instr) {
- char opcodestring[10];
- if (opcode(instr) == ADD) {
-      strcpy(opcodestring, "add");
- } else if (opcode(instr) == NAND) {
-      strcpy(opcodestring, "nand");
- } else if (opcode(instr) == LW) {
-      strcpy(opcodestring, "lw");
- } else if (opcode(instr) == SW) {
-      strcpy(opcodestring, "sw");
- } else if (opcode(instr) == BEQ) {
-      strcpy(opcodestring, "beq");
- } else if (opcode(instr) == JALR) {
-      strcpy(opcodestring, "jalr");
- } else if (opcode(instr) == HALT) {
-      strcpy(opcodestring, "halt");
- } else if (opcode(instr) == NOOP) {
-      strcpy(opcodestring, "noop");
- } else {
-      strcpy(opcodestring, "data");
- }
+     char opcodestring[10];
+     if (opcode(instr) == ADD) {
+         strcpy(opcodestring, "add");
+     } else if (opcode(instr) == NAND) {
+         strcpy(opcodestring, "nand");
+     } else if (opcode(instr) == LW) {
+         strcpy(opcodestring, "lw");
+     } else if (opcode(instr) == SW) {
+         strcpy(opcodestring, "sw");
+     } else if (opcode(instr) == BEQ) {
+         strcpy(opcodestring, "beq");
+     } else if (opcode(instr) == JALR) {
+         strcpy(opcodestring, "jalr");
+     } else if (opcode(instr) == HALT) {
+         strcpy(opcodestring, "halt");
+     } else if (opcode(instr) == NOOP) {
+         strcpy(opcodestring, "noop");
+     } else {
+         strcpy(opcodestring, "data");
+     }
 
- if(opcode(instr) == ADD || opcode(instr) == NAND){
-      printf("%s %d %d %d\n", opcodestring, field2(instr), field0(instr), field1(instr));
- }else if(0 == strcmp(opcodestring, "data")){
-      printf("%s %d\n", opcodestring, signextend(field2(instr)));
- }else{
-      printf("%s %d %d %d\n", opcodestring, field0(instr), field1(instr), signextend(field2(instr)));
- }
+     if(opcode(instr) == ADD || opcode(instr) == NAND){
+         printf("%s %d %d %d\n", opcodestring, field2(instr), field0(instr), field1(instr));
+     }else if(0 == strcmp(opcodestring, "data")){
+         printf("%s %d\n", opcodestring, signextend(field2(instr)));
+     }else{
+         printf("%s %d %d %d\n", opcodestring, field0(instr), field1(instr), signextend(field2(instr)));
+     }
 }
 
 void printstate(statetype *stateptr){
- int i;
- printf("\n@@@\nstate before cycle %d starts\n", stateptr->cycles);
- printf("\tpc %d\n", stateptr->pc);
+     int i;
+     printf("\n@@@\nstate before cycle %d starts\n", stateptr->cycles);
+     printf("\tpc %d\n", stateptr->pc);
 
- printf("\tdata memory:\n");
-      for (i=0; i<stateptr->numMemory; i++) {
-        printf("\t\tdatamem[ %d ] %d\n", i, stateptr->datamem[i]);
-      }
- printf("\tregisters:\n");
-      for (i=0; i<NUMREGS; i++) {
-        printf("\t\treg[ %d ] %d\n", i, stateptr->reg[i]);
-}
- printf("\tIFID:\n");
-      printf("\t\tinstruction ");
-      printinstruction(stateptr->IFID.instr);
-      printf("\t\tpcplus1 %d\n", stateptr->IFID.pcplus1);
- printf("\tIDEX:\n");
-      printf("\t\tinstruction ");
-      printinstruction(stateptr->IDEX.instr);
-      printf("\t\tpcplus1 %d\n", stateptr->IDEX.pcplus1);
-      printf("\t\treadregA %d\n", stateptr->IDEX.readregA);
-      printf("\t\treadregB %d\n", stateptr->IDEX.readregB);
-      printf("\t\toffset %d\n", stateptr->IDEX.offset);
- printf("\tEXMEM:\n");
-      printf("\t\tinstruction ");
-      printinstruction(stateptr->EXMEM.instr);
-      printf("\t\tbranchtarget %d\n", stateptr->EXMEM.branchtarget);
-      printf("\t\taluresult %d\n", stateptr->EXMEM.aluresult);
-      printf("\t\treadregB %d\n", stateptr->EXMEM.readreg);
- printf("\tMEMWB:\n");
-      printf("\t\tinstruction ");
-      printinstruction(stateptr->MEMWB.instr);
-      printf("\t\twritedata %d\n", stateptr->MEMWB.writedata);
- printf("\tWBEND:\n");
-      printf("\t\tinstruction ");
-      printinstruction(stateptr->WBEND.instr);
-      printf("\t\twritedata %d\n", stateptr->WBEND.writedata);
+     printf("\tdata memory:\n");
+          for (i=0; i<stateptr->numMemory; i++) {
+            printf("\t\tdatamem[ %d ] %d\n", i, stateptr->datamem[i]);
+          }
+     printf("\tregisters:\n");
+          for (i=0; i<NUMREGS; i++) {
+            printf("\t\treg[ %d ] %d\n", i, stateptr->reg[i]);
+          }
+     printf("\tIFID:\n");
+          printf("\t\tinstruction ");
+          printinstruction(stateptr->IFID.instr);
+          printf("\t\tpcplus1 %d\n", stateptr->IFID.pcplus1);
+     printf("\tIDEX:\n");
+          printf("\t\tinstruction ");
+          printinstruction(stateptr->IDEX.instr);
+          printf("\t\tpcplus1 %d\n", stateptr->IDEX.pcplus1);
+          printf("\t\treadregA %d\n", stateptr->IDEX.readregA);
+          printf("\t\treadregB %d\n", stateptr->IDEX.readregB);
+          printf("\t\toffset %d\n", stateptr->IDEX.offset);
+     printf("\tEXMEM:\n");
+          printf("\t\tinstruction ");
+          printinstruction(stateptr->EXMEM.instr);
+          printf("\t\tbranchtarget %d\n", stateptr->EXMEM.branchtarget);
+          printf("\t\taluresult %d\n", stateptr->EXMEM.aluresult);
+          printf("\t\treadregB %d\n", stateptr->EXMEM.readreg);
+     printf("\tMEMWB:\n");
+          printf("\t\tinstruction ");
+          printinstruction(stateptr->MEMWB.instr);
+          printf("\t\twritedata %d\n", stateptr->MEMWB.writedata);
+     printf("\tWBEND:\n");
+          printf("\t\tinstruction ");
+          printinstruction(stateptr->WBEND.instr);
+          printf("\t\twritedata %d\n", stateptr->WBEND.writedata);
 }
 
 int signextend(int num){
@@ -228,3 +235,27 @@ int signextend(int num){
   }
   return(num);
 }
+void IFStage(state* state, state* newstate) {
+    /*
+     * Grab instruction from memory and store in new state's pipeline register for IF
+     */
+    newstate.IFID.pcplus1 = state.pc + 1;
+    newstate.IFID.instr = state.datamem[state.pc]
+}
+void IDStage(state* state, state* newstate) {
+    /*
+     * Decode instruction and store in
+     */
+    newstate.IDEX.instr = state.datamem[state.pc]
+    newstate.IDEX.pcplus1 = state.pc + 1
+    newstate.IDEX.readregA = 0; //??? What to put here?
+    newstate.IDEX.readregB = 0; //??? What to put here?
+    newstate.IDEX.offset = 0; //??? What to put here?
+}
+void EXStage(state* state, state* newstate) {
+}
+void MEMStage(state* state, state* newstate) {
+}
+void WBStage(state* state, state* newstate) {
+}
+
